5ca0b6820d42d18d1b39b0323f122cacf256bd53
Valdemar Ceccon <valdemar.ceccon@objective.com.br>
2019-08-09 13:44:13 -0300

    implemented quadrant getter

--- solver.go
+++ solver.go
@@ -17,3 +17,16 @@ func (p Puzzle) ColumnAt(at int) (column Line) {
 	}
 	return
 }
+func (p Puzzle) QuadrantAt(at int) (quadrant Line) {
+
+	quadrant[0] = p[0][6]
+	quadrant[1] = p[0][7]
+	quadrant[2] = p[0][8]
+	quadrant[3] = p[1][6]
+	quadrant[4] = p[1][7]
+	quadrant[5] = p[1][8]
+	quadrant[6] = p[2][6]
+	quadrant[7] = p[2][7]
+	quadrant[8] = p[2][8]
+	return
+}
--- solver_test.go
+++ solver_test.go
@@ -26,22 +26,39 @@ func TestConstraints(t *testing.T) {
 		gotLine := solver.Puzzle.LineAt(1)
 		expectedLine := sudoku_solver.Line{4, 6, 8, 3, 9, 1, 5, 7, 2}
 
-		if !reflect.DeepEqual(gotLine, expectedLine) {
-			t.Errorf("want %v got %v", gotLine, expectedLine)
-		}
-
+		assertLine(t, gotLine, expectedLine)
 	})
 
 	t.Run("given a puzzle, get needed column", func(t *testing.T) {
 		solver := sudoku_solver.Solver{Puzzle: solvedPuzzle}
 
 		gotLine := solver.Puzzle.ColumnAt(1)
-
 		expectedLine := sudoku_solver.Line{3, 6, 2, 1, 9, 4, 8, 7, 5}
 
-		if !reflect.DeepEqual(gotLine, expectedLine) {
-			t.Errorf("want %v got %v", gotLine, expectedLine)
-		}
-
+		assertLine(t, gotLine, expectedLine)
 	})
+
+	t.Run("given a puzzle, get second 'quadrant'", func(t *testing.T) {
+		solver := sudoku_solver.Solver{Puzzle: solvedPuzzle}
+
+		gotQuadrant := solver.Puzzle.QuadrantAt(2)
+		expectedQuadrant := sudoku_solver.Line{6, 8, 1, 5, 7, 2, 3, 4, 9}
+
+		assertLine(t, gotQuadrant, expectedQuadrant)
+	})
+
+	t.Run("given a puzzle, get middle 'quadrant'", func(t *testing.T) {
+		solver := sudoku_solver.Solver{Puzzle: solvedPuzzle}
+
+		gotQuadrant := solver.Puzzle.QuadrantAt(4)
+		expectedQuadrant := sudoku_solver.Line{7, 3, 6, 5, 4, 2, 9, 1, 8}
+
+		assertLine(t, gotQuadrant, expectedQuadrant)
+	})
+}
+
+func assertLine(t *testing.T, gotLine sudoku_solver.Line, expectedLine sudoku_solver.Line) {
+	if !reflect.DeepEqual(gotLine, expectedLine) {
+		t.Errorf("want %v got %v", gotLine, expectedLine)
+	}
 }
